c5cd4f029618c113946bed0debad89664d101a59
andree93 <34599817+andree93@users.noreply.github.com>
2021-11-13 14:19:02 +0100

    Add files via upload
    pulizia codice

--- main.py
+++ main.py
@@ -13,36 +13,25 @@ alfabeto_inglese = {"A":0, "B":1, "C":2, "D":3, "E":4, "F":5, "G":6, "H":7, "I":
 
 def genera_cifrario(alfabeto_inglese, modulo, chiave):
     alfabeto_inglese_cifrato = [ list(alfabeto_inglese.keys())[((indice_lettera + chiave) % modulo)] for indice_lettera in list(alfabeto_inglese.values()) ]
-
     dizionario_alfabeto_inglese_cifrato = {}
-
     if (len(alfabeto_inglese) == len(alfabeto_inglese_cifrato)):
-
         dizionario_alfabeto_inglese_cifrato = {key: value for key, value in zip(list(alfabeto_inglese.keys()), alfabeto_inglese_cifrato)}
     else:
         print("le liste hanno dimensioni differenti!") #test
             
-
     return dizionario_alfabeto_inglese_cifrato
 
 
 
 def cifra(alfabeto_inglese, modulo, chiave, parola ):
-
     dizionario_alfabeto_inglese_cifrato = genera_cifrario(alfabeto_inglese, modulo, chiave)
-
-
     parola_cifrata = [dizionario_alfabeto_inglese_cifrato[lettera.upper()] for lettera in parola.upper()]
-
     counter = 0
-
     for lettera in parola.upper():
         indice_c = (alfabeto_inglese[lettera] + chiave) % modulo
         print("Lettera: "+lettera + " "+"Numero: "+ str(alfabeto_inglese[lettera]) + " " + "Congruo a: " + str( indice_c ) + " " +str() + "Lettera: "+ parola_cifrata[counter] + " " + "(" + str(alfabeto_inglese[lettera]) + "+" + str(chiave) + " modulo " + str(modulo) + ")")
         counter = counter+1
-
     str_parola_cifrata = "".join(parola_cifrata)
-
     return  str_parola_cifrata
 
 
